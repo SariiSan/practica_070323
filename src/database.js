4fb52a146083b4e97a0b4dfe80377044bb3275a4
SariiSan <jandy.17sms@gmail.com>
2021-11-13 09:44:39 -0600

    ok

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host:'us-cdbr-east-04.cleardb.com',
-    user: 'bcc633eac38a26',
-    password:'37ac9c6d',
-    database:'heroku_8cfa907bd831ff0',
+    host:'sql10.freemysqlhosting.net',
+    user: 'sql10450762',
+    password:'8KwEnBIml2',
+    database:'sql10450762',
     port:'3306',
     multipleStatements: true
 
